f1d455881a631d078497116e6bc3b3938c5ad092
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2024-05-05 14:26:22 +0900

    feat:plotly 수정

--- pages/plotly.py
+++ pages/plotly.py
@@ -1,20 +1,14 @@
 import streamlit as st
 import numpy as np
-import plotly.figure_factory as ff
+import plotly.express as px
+import seaborn as sns
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
 
-# Group data together
-hist_data = [x1, x2, x3]
+df = sns.load_dataset("titanic")
+st.write(df.head())
+df_plotly = df.pivot_table(index='pclass', values=['fare'], aggfunc='mean').reset_index()
+fig = px.bar(df_plotly, x = 'pclass', y = ['fare'],  barmode = 'group')
 
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
+st.plotly_chart(fig, use_container_width=True)
+fig = px.box(df, x='survived', y='age')
 st.plotly_chart(fig, use_container_width=True)
